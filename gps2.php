3a55aec73f98cb1d7594ce87ab27c54385a18cae
Douglas Evink <devink@specialized-computers.com>
2013-06-10 07:09:11 -0400

    Page Refresh

--- gps2.php
+++ gps2.php
@@ -1,6 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>GPS Tracker</title>
+  <meta name="description" content="BigFish">
+  <meta name="author" content="ME">
+  <meta http-equiv="refresh" content="30; ,URL=gps.php">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<body>
+
 <?php
 // var_dump($_GET);
 echo $_GET["lat"];
 echo "<br />";
 echo $_GET["long"];
 ?>
+
+
+</body>
+</html>
+
+
